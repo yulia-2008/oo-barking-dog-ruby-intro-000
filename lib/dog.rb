ac2d218ac9b4f488af30307ce56d545e08954e38
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-24 16:31:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,11 @@
 
 class Dog 
+  def initialize(name,bark)
+    @name=name
+    @bark=bark
+  end
 
-def name=(dog_name)
+=begin def name=(dog_name)
   @name=dog_name
 end
 def bark=(bark)
@@ -10,7 +14,10 @@ end
   
 
 end
-fido=Dog.new
+
+
+#fido=Dog.new
 fido.name="Fido"
 puts fido.name
 puts fido.bark
+=end
