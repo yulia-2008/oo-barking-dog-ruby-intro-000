4cb97c71a8307b68eee0f0225edfee38b3d82e45
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-24 16:28:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+
+class Dog 
+
+def name=(dog_name)
+  @name=dog_name
+end
+def bark=(bark)
+  @bark="Woof!"
+end
+  
+
+end
+fido=Dog.new
+fido.name="Fido"
+puts fido.name
+puts fido.bark
